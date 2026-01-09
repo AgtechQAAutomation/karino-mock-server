50f149178dc1029b785f0646052f668d0a114f5f
Kavushik Rajappan <kavushik.rajappan@kjbnlabs.in>
2026-01-09 13:31:34 +0530

    Your commit message

--- controllers/delivery.controller.go
+++ controllers/delivery.controller.go
@@ -5,15 +5,15 @@ import (
 	"math"
 	// "time"
 	"github.com/gofiber/fiber/v2"
-	"github.com/shyamsundaar/karino-mock-server/models/delivery"
+	// "github.com/shyamsundaar/karino-mock-server/models/delivery"
 	// "gorm.io/gorm"
 	// "github.com/gin-gonic/gin"
 	"github.com/shyamsundaar/karino-mock-server/initializers"
-	// "github.com/shyamsundaar/karino-mock-server/models/delivery"
+	"github.com/shyamsundaar/karino-mock-server/models/sales"
 )
 
-// GetDeliveryDetailHandler handles GET /spic_to_erp/customers/:coopId/salesorders/deliverydocuments
-// @Summary      List deliverydocuments updated within date ranges
+// GetCustomerDeliveryDocumentDetailHandler handles GET /spic_to_erp/customers/:coopId/salesorders/deliverydocuments
+// @Summary      List salesorder updated within date ranges
 // @Description  Get a paginated list of farmer details for a specific cooperative
 // @Tags         deliverydocuments
 // @Accept       json
@@ -23,11 +23,11 @@ import (
 // @Param        updatedTo     query     string  false  " "
 // @Param        page          query     int     false  "Page number"    default(1)
 // @Param        limit         query     int     false  "Items per page" default(10)
-// @Success      200    {object}  delivery.ListDeliveryDocumentsResponse
+// @Success      200    {object}  sales.ListSalesOrderResponse
 // @Router       /spic_to_erp/customers/{coopId}/salesorders/deliverydocuments [get]
-func GetDeliveryDetailWithinDateHandler(c *fiber.Ctx) error {
+func GetCustomerDeliveryDocumentDetailHandler(c *fiber.Ctx) error {
 	coopId := c.Params("coopId")
-	var deliverydocuments []delivery.DeliverydocumentsListResponse
+	var salesorder []sales.SalesOrder
 
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 	limit, _ := strconv.Atoi(c.Query("limit", "10"))
@@ -35,7 +35,7 @@ func GetDeliveryDetailWithinDateHandler(c *fiber.Ctx) error {
 	var totalRecords int64
 
 	query := initializers.DB.
-		Model(&delivery.DeliverydocumentsListResponse{}).
+		Model(&sales.SalesOrder{}).
 		Where("coop_id = ?", coopId)
 
 	query.Count(&totalRecords)
@@ -43,9 +43,9 @@ func GetDeliveryDetailWithinDateHandler(c *fiber.Ctx) error {
 	if err := query.
 		Limit(limit).
 		Offset(offset).
-		Find(&deliverydocuments).Error; err != nil {
+		Find(&salesorder).Error; err != nil {
 
-		return c.Status(fiber.StatusBadGateway).JSON(delivery.ErrorDeliverydocumentsResponse{
+		return c.Status(fiber.StatusBadGateway).JSON(sales.ErrorSalesOrderResponse{
 			Success: false,
 			Message: err.Error(),
 		})
@@ -53,22 +53,21 @@ func GetDeliveryDetailWithinDateHandler(c *fiber.Ctx) error {
 
 	totalPages := int(math.Ceil(float64(totalRecords) / float64(limit)))
 
-	var data []delivery.DeliverydocumentsListResponse
-	for _, f := range deliverydocuments {
-		data = append(data, delivery.DeliverydocumentsListResponse{
-			// CoopID:              f.CoopID,
-			TempERPSalesOrderId: f.TempERPSalesOrderId,
+	var data []sales.SalesOrderListResponse
+	for _, f := range salesorder {
+		data = append(data, sales.SalesOrderListResponse{
+			TempERPSalesOrderId: f.TempID,
 			ErpSalesOrderId:     f.ErpSalesOrderId,
 			ErpSalesOrderCode:   f.ErpSalesOrderCode,
-			SpicSalesOrderId:    f.SpicSalesOrderId,
-			// CreatedAt:           f.CreatedAt.Format("2006-01-02T15:04:05Z"),
-			// UpdatedAt:           f.UpdatedAt.Format("2006-01-02T15:04:05Z"),
+			SpicSalesOrderId:    f.OrderID,
+			CreatedAt:           f.CreatedAt.Format("2006-01-02T15:04:05Z"),
+			UpdatedAt:           f.UpdatedAt.Format("2006-01-02T15:04:05Z"),
 		})
 	}
 
-	return c.Status(fiber.StatusOK).JSON(delivery.ListDeliveryDocumentsResponse{
+	return c.Status(fiber.StatusOK).JSON(sales.ListSalesOrderResponse{
 		Data: data,
-		Pagination: delivery.PaginationInfo{
+		Pagination: sales.PaginationInfo{
 			Page:        page,
 			Limit:       limit,
 			TotalItems:  int(totalRecords),
@@ -77,8 +76,6 @@ func GetDeliveryDetailWithinDateHandler(c *fiber.Ctx) error {
 			HasNext:     page < totalPages,
 		},
 	})
-	// return c.Status(fiber.StatusCreated).JSON(response)
-
 }
 
 
